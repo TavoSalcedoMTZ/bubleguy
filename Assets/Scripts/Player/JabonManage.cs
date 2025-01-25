ea64d4a17f69e5c4ff7d86b16d614c7f2f29c2df
TavoSalcedoMTZ <150973163+TavoSalcedoMTZ@users.noreply.github.com>
2025-01-24 21:30:32 -0600

    Ahora se Escala

--- Assets/Scripts/Player/JabonManage.cs
+++ Assets/Scripts/Player/JabonManage.cs
@@ -4,33 +4,48 @@ using UnityEngine;
 
 public class JabonManage : MonoBehaviour
 {
-    public int Jabon=15;
+    public int Jabon = 15;
     private DisparoPlayer player;
+    public Transform playerTransf;
+    private Vector3 initialScale;
+    private float maxJabon = 15f;  // Valor máximo de Jabón
 
     void Start()
     {
-        player=GetComponent<DisparoPlayer>();
-
+        player = GetComponent<DisparoPlayer>();
+        initialScale = playerTransf.localScale; // Guarda la escala inicial
     }
 
     void Update()
     {
-
         if (Jabon <= 0)
         {
             Debug.Log("Te has quedado sin Jabon");
             player.canShoot = false;
         }
-        
     }
 
     public void JabonDicrese(int _cantidadjabon)
     {
-         Jabon=Jabon-_cantidadjabon;
+        Jabon -= _cantidadjabon;
+
+        Jabon = Mathf.Max(Jabon, 0);
+
+ 
+        float scaleFactor = Mathf.InverseLerp(0, maxJabon, Jabon);
+
+        playerTransf.localScale = Vector3.Lerp(Vector3.zero, initialScale, scaleFactor);
     }
 
     public void JabonIncremense(int _cantidadjabon)
     {
-        Jabon=Jabon+_cantidadjabon;
+        Jabon += _cantidadjabon;
+
+        Jabon = Mathf.Min(Jabon, (int)maxJabon);
+
+
+        float scaleFactor = Mathf.InverseLerp(0, maxJabon, Jabon);
+      
+        playerTransf.localScale = Vector3.Lerp(Vector3.zero, initialScale, scaleFactor);
     }
 }
